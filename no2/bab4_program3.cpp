03088a6e0b28e4bab412c5fca72019557c2cd7ca
Revaalfian <lolamonda10@gmail.com>
2018-10-11 23:14:02 +0700

    Add files via upload

---
+++ no2/bab4_program3.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main(){
+
+	void QuickSort(Array [A], int L, int L)
+	{
+	if L<N;
+		M:Partition(A,L,N)
+		QuickSort(A,L,M-1)
+		QuickSort(A,M+1,N)
+		endif
+	}
+	void Partition(Array [A],int L,int N)
+	{
+		select M,where L<=M<=N;
+		reorder A(L)...A(N)so that l<M implies A(I)<=A(M),and I>M implies A(I)>= A=(M)
+		return M
+	}
+}
